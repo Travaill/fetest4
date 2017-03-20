9b0d786e2615547cc99f969eb16cdf504941bc0c
Travaill <2329677945@qq.com>
2017-03-20 22:20:50 +0800

    second commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,23 @@
     'use strict';
     var sidebar=$('#SideBar'),
      sidebar_trigger=$('#menubotton'),
-     mask=$('#Mask');
+     mask=$('#Mask'),
+     currYear = (new Date()).getFullYear(),
+   	 opt={};
+     opt.date = {preset : 'date'};
+     opt.datetime = {preset : 'datetime'};
+     opt.time = {preset : 'time'};
+     opt.default = {
+       theme: 'android-ics light', //皮肤样式
+       display: 'modal', //显示方式
+       mode: 'scroller', //日期选择模式
+       dateFormat: 'yyyy-mm-dd',
+       lang: 'zh',
+       showNow: true,
+       nowText: "今天",
+       startYear: currYear - 50, //开始年份
+       endYear: currYear + 10 //结束年份
+     };
      function showSideBar()
      {
          sidebar.css('left',0);
@@ -17,4 +33,5 @@
      }
     sidebar_trigger.on('click',showSideBar);
     mask.on('click',hideSideBar);
+    $("#USER_AGE").mobiscroll($.extend(opt['date'], opt['default']))
   })
